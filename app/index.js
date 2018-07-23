764408c12d41d05cdbba875d7860983ad8eaa40a
kimsunmee <sunmee.geem@navercorp.com>
2018-07-23 19:03:12 +0900

    test

--- app/index.js
+++ app/index.js
@@ -2,25 +2,22 @@ import _ from 'lodash';
 import '../base.css';
 // import { isMoment } from '../node_modules/moment';
 var moment = require('moment');
-var ele = document.querySelectorAll('p');
-var ele2 = document.getElementsByClassName('container')[0];
+// var ele = document.querySelectorAll('p');
+// var ele2 = document.getElementsByClassName('container')[0];
 var $ = require('jquery');
 console.log("loaded jQuery version is " + $.fn.jquery);
 
-
-function component () {
-    var element = document.createElement('div');
-  
-    /* lodash is required for the next line to work */
-    element.innerHTML = _.join(['Hello','webpack'], ' ');
-  
-    return element;
+const inputData = document.getElementById('todo').value;
+// const listItem = document.getElementsByClassName('todoLink')[i];  
+function todoWriteSubmit () {
+  const listItem = document.getElementsByClassName('todoItem')[0];
+  return listItem  
 }
   
-document.body.appendChild(component());
+document.getElementsByClassName('todoList').appendChild(todoWriteSubmit ());
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  ele[0].innerHTML = moment().format();
-  ele[1].innerHTML = _.drop([1,2,3],0);
-  ele2.innerHTML = "webpack loader!!!!!!!!!";
+  // ele[0].innerHTML = moment().format();
+  // ele[1].innerHTML = _.drop([1,2,3],0);
+  // ele2.innerHTML = "webpack loader!!!!!!!!!";
 });
