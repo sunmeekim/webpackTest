732362404197bc300e3f1536a081bc2204267b6b
kimsunmee <sunmee.geem@navercorp.com>
2018-07-26 18:42:36 +0900

    기능별 파일 나누기

--- app/index.js
+++ app/index.js
@@ -1,60 +1,51 @@
 import _ from 'lodash';
 import '../base.css';
-// import { isMoment } from '../node_modules/moment';
-var moment = require('moment');
-// var ele = document.querySelectorAll('p');
-// var ele2 = document.getElementsByClassName('container')[0];
-var $ = require('jquery');
-console.log("loaded jQuery version is " + $.fn.jquery);
-
+import moment from 'moment';
+import $ from 'jquery';
 
 const todoList = () => {
-  
   const todoWrap = document.getElementById('todoCont');
-  const todoList = '<ul class=\"todoList\" id=\"todoList\"></ul>';
-  todoWrap.innerHTML = todoList;
-
-  return 
+  const todoList = document.createElement('ul');
+  todoList.id = 'todoList';
+  todoList.className = 'todoList';
+  todoWrap.appendChild(todoList);
 }
+
 const todoItem = () => {
+  todoList(); //ul태그 만들어주기
+  let defaultData = []; // input에 입력된 값을 배열로 받을 예정
+  let inputData = document.getElementById('todo').value; //input에 입력된 값
   
-  const inputData = document.getElementById('todo').value;
-  const itemLink = document.createElement('a');
-  itemLink.className = 'todoLink';
-  itemLink.href = '#'
-  itemLink.innerHTML = inputData;
-  const item  = document.createElement('li');
-  item.className = 'todoItem';
-// const item = '<li class=\"todoItem\"><a href=\"#\" class=\"todoLink\">'+ inputData +'</a></li>'
+  defaultData.push(inputData);// 배열에 input에서 받은 값을 추가
+  let list = document.querySelector('ul.todoList'); // 위에서 만들어준 ul 태그를 잡아줌
 
-  document.getElementById("todoList").appendChild(item).appendChild(itemLink);
+  //입력된 값의 갯수만큼 li에 값을 추가해줍니다. 
+  defaultData.forEach(function(index){
+    
+    let itemLink = document.createElement('a');
+    itemLink.className = 'todoLink';
+    itemLink.href = '#'
+    itemLink.textContent = inputData;
+
+    let item  = document.createElement('li');
+    item.className = 'todoItem';
+
+    list.appendChild(item).appendChild(itemLink);
+
+  })
+
+  //return defaultData;
 }
-
-// const todoCheck = () => {
-//   var checkPoint = document.querySelector('.todoItem').classList.contains('done');
-//   if(checkPoint) {
-//     document.querySelector('.todoItem').setAttribute("class","todoItem done")
-//   } else {
-//     document.querySelector('.todoItem').setAttribute("class","todoItem")
-//   }
-
-// } 
-// const todoLink = document.querySelector('.todoLink');
-// todoLink.addEventListener ("click", function(e){
-//   e.preventDefault();
-//   todoCheck();
-// });
 
 const inputBtn = document.getElementById('todoBtn');
 const resetBtn = document.getElementById('todoResetBtn');
 
-inputBtn.addEventListener("click", function(e){
-  e.preventDefault()
-  todoList();
+inputBtn.addEventListener("click",function(){
   todoItem();
-  
 });
+
 resetBtn.addEventListener("click", function(e) {
   e.preventDefault()
-  document.getElementsByClassName("todoList")[0].remove();
+  
+  document.getElementById("todoList").remove();
 });
